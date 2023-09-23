49cadbe9e99a545fc00de1e533660a4e16311108
徐志斌 <1262254123@qq.com>
2023-09-23 18:45:46 +0800

    DockerBiu-Backend

---
+++ biu-user/src/test/java/com/example/biuuser/BiuUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.biuuser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiuUserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
