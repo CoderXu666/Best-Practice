8abbbe91edb169019d4cca48d77f543e4fd34baa
徐志斌 <1262254123@qq.com>
2023-09-24 09:46:23 +0800

    Code Commit By 徐志斌

---
+++ biu-common/src/main/java/com/biu/enums/ResponseEnum.java
@@ -0,0 +1,52 @@
+package com.biu.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+/**
+ * @Author 徐志斌
+ * @Date: 2023/5/8 22:00
+ * @Version 1.0
+ * @Description: 响应封装类状态码 + 响应信息
+ */
+@Getter
+@AllArgsConstructor
+public enum ResponseEnum {
+    /**
+     * 通用
+     */
+    SUCCESS(200, "操作成功"),
+    FAIL(500, "操作失败"),
+    NO_ARGS(50, "参数信息不存在，系统出现异常"),
+    NO_STRATEGY_HANDLER(50, "Map中不存在当前策略类"),
+    FILE_NOT_EXIST(50, "当前上传文件不存在"),
+
+
+    /**
+     * 自定义
+     */
+    /*---------------------------------USER------------------------------*/
+    TOKEN_EXPIRED(5000, "用户Token已失效，请重新登录"),
+    TOKEN_NOT_EXIST(5000, "用户Token信息不存在"),
+
+    USER_NOT_EXIST(5000, "未查询到当前用户相关信息"),
+    USER_EXIST(5000, "当前账号已存在，换个试试"),
+    USER_LONG(5000, "当前账号长度太长！"),
+    USER_SHORT(5000, "当前账号长度太短！"),
+
+    CAPTCHA_ERROR(5000, "验证码输入错误，请重试"),
+    CAPTCHA_NOT_EXIST(5000, "验证码不存在，请刷新页面重新获取"),
+
+    PASSWORD_ERROR(5000, "密码输入错误，请重试"),
+    PASSWORD_DIFF(5000, "两次密码输入不一致，请重试"),
+    PASSWORD_LONG(5000, "密码长度太长！"),
+    PASSWORD_SHORT(5000, "密码长度太短！"),
+    ;
+
+    /*---------------------------------COMMUNITY------------------------------*/
+    /*---------------------------------IM------------------------------*/
+    /*---------------------------------PARTY------------------------------*/
+
+    private Integer code;
+    private String msg;
+}
---
+++ biu-user/src/main/java/com/biu/controller/UserController.java
@@ -0,0 +1,22 @@
+package com.biu.controller;
+
+import com.biu.enums.ResponseEnum;
+import com.biu.response.R;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author 徐志斌
+ * @Date: 2023/9/24 9:16
+ * @Version 1.0
+ * @Description: UserController
+ */
+@RestController
+@RequestMapping("/user")
+public class UserController {
+    @PostMapping("/login")
+    public R login() {
+        return R.out(ResponseEnum.SUCCESS, null);
+    }
+}
